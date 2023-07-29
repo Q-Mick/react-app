891c1d85bb7302312a4eb245b731f26b006391e0
Q-Mick <quentinmick@protonmail.com>
2023-07-29 16:38:58 -0600

    added react MouseEvent handle func

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,21 +1,25 @@
+import { MouseEvent } from "react"
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]
-  items = []
-  if (items.length === 0) {
-    return (
-      <>
-        <h1>List</h1>
-        <p>No items found</p>
-      </>
-    )
-  }
+
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p> No items </p> : null
+  // }
+
+  // EVENT HANDLER
+  const handleClick = (event: MouseEvent) => console.log(event)
 
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p> No items </p> : null} */}
+      {items.length === 0 && <p> No items </p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
