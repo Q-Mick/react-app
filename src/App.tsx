df401f94d9cc3806474ff1f09f2f19fcc1d13901
Q-Mick <quentinmick@protonmail.com>
2023-07-31 00:01:42 -0600

    react testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,22 @@ import { useState } from "react"
 
 function App() {
 	const [alertVisible, setAlertVisibility] = useState(false)
-
+	let items = ["Boise", "Meridian", "Eagle", "Middleton", "Nampa"]
+	const handleSelectItem = (item: string) => {
+		console.log(item)
+	}
 	return (
 		<div>
 			{alertVisible && (
 				<Alert onClose={() => setAlertVisibility(false)}>Hello World</Alert>
 			)}
 			<Button onClick={() => setAlertVisibility(true)}>Show alert</Button>
+			<h1></h1>
+			<ListGroup
+				items={items}
+				heading="Cities"
+				onSelectItem={handleSelectItem}
+			/>
 		</div>
 	)
 }
